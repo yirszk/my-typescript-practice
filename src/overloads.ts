3e58a7677ab074fef654437c627cd6b13ccca5d5
Yoichiro Suzuki <y.suzuki@hey.com>
2022-07-02 13:56:36 +0900

    overload

---
+++ src/overloads.ts
@@ -0,0 +1,21 @@
+export {};
+
+// シグネチャー
+function double(value: number): number;
+function double(value: string): string;
+
+// 型宣言はシグネチャー側で行うので、関数実体ではany型でいい
+function double(value: any): any {
+  console.log(typeof value);
+  if (typeof value === 'number') {
+    return value * 2;
+  } else if (typeof value === 'string') {
+    return value + value;
+  } else {
+    throw '引数の型を確認してください'
+  }
+}
+
+console.log(double(100));
+console.log(double('Go '));
+// console.log(double(true)); // シグネチャーにbooleanを指定していないので実行されない
