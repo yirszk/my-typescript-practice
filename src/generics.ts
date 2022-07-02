daa652b5ae7405472d39a2e39648f07cedd674e8
Yoichiro Suzuki <y.suzuki@hey.com>
2022-07-03 00:23:42 +0900

    generics

---
+++ src/generics.ts
@@ -0,0 +1,23 @@
+export {};
+
+// 関数
+const echo = <T>(arg: T): T => {
+  return arg;
+};
+
+console.log(echo<number>(100));
+console.log(echo<string>('Hello!'));
+console.log(echo<boolean>(true));
+
+// クラス
+class Mirror<T> {
+  constructor(public value: T) {}
+
+  echo(): T {
+    return this.value;
+  }
+}
+
+console.log(new Mirror<number>(123).echo());
+console.log(new Mirror<string>('foo').echo());
+console.log(new Mirror<boolean>(true).echo());
